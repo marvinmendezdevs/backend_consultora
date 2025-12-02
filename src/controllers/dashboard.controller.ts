e3ec186afc1be873891cca32a7a18566cd17c992
marvinmendezlearn <marvinmendez.learn@gmail.com>
2025-12-02 16:37:18 -0600

    Update dashboard.controller.ts

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,5 +1,23 @@
+import { prisma } from "@/config/database";
 import { Request, Response, NextFunction } from "express";
 
 export const dashboard = async (req: Request, res: Response, next: NextFunction) => {
-    res.send(req.user);
+    const user = req.user!;
+
+    try {
+        const [clients] = await Promise.all([
+            prisma.clients.count({
+                where: {
+                    companyId: user.companyId,
+                }
+            }),
+        ]);
+
+        res.json({
+            clients
+        });
+        
+    } catch (error) {
+        return res.status(500).json({msg: "Error en el servidor"});
+    }
 }
